7d4863fc4b340641ee4f380b13f58a3ade5a1131
NUTT1101 <zhangf099@gmail.com>
2022-11-19 21:18:58 +0800

    api update

--- src/main/java/org/dyu5thdorm/models/Room.java
+++ src/main/java/org/dyu5thdorm/models/Room.java
@@ -1,4 +1,11 @@
 package org.dyu5thdorm.models;
 
 public record Room(String roomTag, Student student) {
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomTag='" + roomTag + '\'' +
+                ", student=" + student +
+                '}';
+    }
 }
